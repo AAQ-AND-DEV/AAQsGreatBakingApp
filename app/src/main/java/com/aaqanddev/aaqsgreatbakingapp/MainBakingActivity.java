6b8470048cf489472f03f1eb66c9a2b1e690e880
AquaticNewKeyHack <aaronquaday@gmail.com>
2018-11-19 14:55:22 -0800

    converted to androidX useAndroidX set to true enableJetifier set to true (3rd party libraries used) dependencies manually changed

--- app/src/main/java/com/aaqanddev/aaqsgreatbakingapp/MainBakingActivity.java
+++ app/src/main/java/com/aaqanddev/aaqsgreatbakingapp/MainBakingActivity.java
@@ -3,11 +3,15 @@ package com.aaqanddev.aaqsgreatbakingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dagger.android.AndroidInjection;
+
 public class MainBakingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setContentView(R.layout.activity_main_baking);
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_baking);
+
     }
 }
